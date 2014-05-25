5bf1a8cf4a201d6b676f757f9b2dab80e095506d
Mike Smith <mike@scgtrp.net>
2014-05-25 13:16:55 -0400

    Give factories the ability to queue units to build.

--- Assets/Scripts/Units/Factory.cs
+++ Assets/Scripts/Units/Factory.cs
@@ -1,46 +1,49 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent (typeof(Entity))]
 public class Factory : MonoBehaviour {
         public Entity[] prefabs;
 
         private DReal delay;
-        private int buildMe;
+
+        private Queue<int> buildQueue;
 
         Entity entity;
 
         void Awake() {
                 entity = GetComponent<Entity>();
                 entity.AddUpdateAction(2, TickUpdate);
-                buildMe = -1;
+                buildQueue = new Queue<int>();
         }
 
         void TickUpdate() {
                 if(delay > 0) {
                         delay -= ComSat.tickRate;
                 }
-                if(delay <= 0 && buildMe != -1) {
+                if(delay <= 0 && buildQueue.Any()) {
                         // Timer expired and we're building something.
+                        var buildMe = buildQueue.Dequeue();
                         print("Build new " + prefabs[buildMe]);
 
                         var rotation = ComSat.RandomRange(0, DReal.TwoPI);
                         var offset = DVector2.FromAngle(rotation) * ComSat.RandomRange(entity.collisionRadius + 5, entity.collisionRadius + 15);
 
                         ComSat.SpawnEntity(entity, prefabs[buildMe].gameObject, entity.position + offset, rotation);
-
-                        buildMe = -1;
+                }
+                if(delay <= 0 && buildQueue.Any()) {
+                        delay = prefabs[buildQueue.Peek()].buildTime;
                 }
         }
 
         void UIAction(int what) {
-                if(delay > 0 || what < 0 || what >= prefabs.Length) {
+                if(what < 0 || what >= prefabs.Length) {
                         return;
                 }
-
-                delay = prefabs[what].buildTime;
-                buildMe = what;
+                buildQueue.Enqueue(what);
+                delay = prefabs[buildQueue.Peek()].buildTime;
         }
 
         private bool isSelected;
@@ -57,15 +60,25 @@ public class Factory : MonoBehaviour {
                 if(!isSelected) return;
 
                 if(delay > 0) {
-                        GUI.backgroundColor = Color.red;
                         GUI.Box(new Rect(10, 10, 64, 25), ((int)Mathf.Ceil((float)delay)).ToString());
                 } else {
                         GUI.Box(new Rect(10, 10, 64, 25), "Ready");
                 }
 
+                var building = buildQueue.Any() ? buildQueue.Peek() : -1;
+
                 for(int i = 0; i < prefabs.Length; ++i) {
+                        GUI.backgroundColor = (i == building && Time.time % 1 < 0.5f) ? Color.yellow : Color.white;
+
                         if(GUI.Button(new Rect(10, 45 + i * 74, 64, 64), prefabs[i].buildIcon)) {
                                 ComSat.IssueUIAction(entity, i);
+                        }
+
+                        GUI.backgroundColor = Color.white;
+
+                        var queued = buildQueue.Count(qi => qi == i);
+                        if (queued > 0) {
+                                GUI.Label(new Rect(10, 45 + i * 74, 64, 24), queued.ToString());
                         }
                 }
         }
