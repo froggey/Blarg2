bdbd2433dad5197d864a7120f7748eb4b230ab08
Mike Smith <mike@scgtrp.net>
2014-06-07 15:28:22 -0400

    magic smoke mines can now produce glowing orbs that ram into enemy units to damage them

---
+++ Assets/Scripts/Units/Orb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(Entity), typeof(SimpleMovable))]
+public class Orb : MonoBehaviour {
+        private Entity entity;
+        private SimpleMovable motor;
+
+        void Awake() {
+                entity = GetComponent<Entity>();
+                entity.AddUpdateAction(TickUpdate);
+                motor = GetComponent<SimpleMovable>();
+        }
+
+        void TickUpdate() {
+                foreach(var ent in ComSat.currentInstance.worldEntityCollisionCache) {
+                        if (ent == entity) continue;
+                        if(ent.team != entity.team && (ent.position - entity.position).sqrMagnitude < ent.collisionRadius * ent.collisionRadius + 4) {
+                                ent.Damage(5);
+                        }
+                }
+        }
+
+        void Attack(Entity target) {
+                motor.Move(target.position);
+        }
+}
